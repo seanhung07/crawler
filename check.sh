2b642df62624a43596b3fcfe9926850fb60780b2
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2019-02-21 23:27:24 +0800

    Add files via upload

--- check.sh
+++ check.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 curl https://bootstrap.pypa.io/get-pip.py > get-pip.py
 sudo python get-pip.py
-pip install beautifulsoup4
-pip install requests
-pip install wget
-pip install pytub
-pip install pyfiglet
-pip install colorama
+sudo pip install beautifulsoup4
+sudo pip install requests
+sudo pip install wget
+sudo pip install pytube
+sudo pip install pyfiglet
+sudo pip install colorama
